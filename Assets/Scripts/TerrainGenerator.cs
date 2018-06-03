88b6fc7bbb1ffc43b734786d5cbc47859ccb2b64
Nathaniel Jew <nathankjew@gmail.com>
2018-06-03 14:46:51 -0700

    Added method for finding lowest height

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -6,7 +6,7 @@ public class TerrainGenerator : MonoBehaviour {
     public static TerrainGenerator instance;
 
     public int mapWidth;
-    public int mapResolution;
+    public float maxHeightDiff;
 
     public MeshFilter terrainMesh;
 
@@ -19,23 +19,31 @@ public class TerrainGenerator : MonoBehaviour {
 	}
 
 	public void GenerateTerrain() {
-        heights = new float[mapWidth * mapResolution];
+        heights = new float[mapWidth];
 
         //initialize points
         for (int i = 0; i < heights.Length; i++) {
-            heights[i] = Random.Range(5f, 10f);
+            //heights[i] = Random.Range(5f, 10f);
+            heights[i] = 5f;
         }
 
         UpdateVisual();
     }
 
-    void UpdateVisual() {
+	/*private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            float xPos = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+            AddMassAtPos(xPos, 1f);
+        }
+	}*/
+
+	void UpdateVisual() {
         int pointCount = heights.Length + 2;
         Vector2[] points2D = new Vector2[pointCount];
         Vector3[] points3D = new Vector3[pointCount];
 
         for (int i = 0; i < heights.Length; i++) {
-            Vector2 point = new Vector2(i / (float)mapResolution, heights[i]);
+            Vector2 point = new Vector2(i, heights[i]);
             points2D[i] = point;
             points3D[i] = new Vector3(point.x, point.y, 0f);
         }
@@ -58,17 +66,53 @@ public class TerrainGenerator : MonoBehaviour {
         terrainMesh.mesh = mesh;
     }
 
+    public int GetLowestHeight(float left, int width) {
+        int index = Mathf.RoundToInt(left);
+        float curMinHeight = Mathf.Infinity;
+
+        for (int i = 0; i < width; i++) {
+            curMinHeight = Mathf.Min(curMinHeight, heights[index + i]);
+        }
+
+        return Mathf.FloorToInt(curMinHeight);
+    }
+
     public PointData GetPointDataAtPos(float xPos) {
-        int leftIndex = Mathf.FloorToInt(xPos * mapResolution);
-        int rightIndex = Mathf.CeilToInt(xPos * mapResolution);
-        float p = (xPos * mapResolution - leftIndex);
+        int leftIndex = Mathf.FloorToInt(xPos);
+        int rightIndex = Mathf.CeilToInt(xPos);
+        float p = (xPos - leftIndex);
 
         float yPos = Mathf.Lerp(heights[leftIndex], heights[rightIndex], p);
-        Vector2 diff = new Vector2(1f / mapResolution, heights[rightIndex] - heights[leftIndex]);
+        Vector2 diff = new Vector2(1f, heights[rightIndex] - heights[leftIndex]);
         float angle = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
 
         return new PointData(yPos, angle);
     }
+
+    /*public void AddMassAtPos(float xPos, float mass) {
+        int index = Mathf.RoundToInt(xPos * mapResolution);
+        AddMassAtIndex(index, mass);
+        
+        UpdateVisual();
+    }
+
+    bool AddMassAtIndex(int index, float mass) {
+        if(index 
+
+        float curHeight = heights[index];
+        float remaininingMass = mass;
+
+        float maxDiff = Mathf.Max(Mathf.Abs(curHeight - heights[index - 1]), Mathf.Abs(curHeight - heights[index + 1]));
+        if (maxDiff < maxHeightDiff) {
+            heights[index] += mass;
+            return 0f;
+        } else {
+            remaininingMass -= maxHeightDiff;
+            AddMassAtIndex(index-1,remaininingMass/2);
+            AddMassAtIndex(index+1,remaininingMass/2);
+            return 0f;
+        }
+    }*/
 
     public struct PointData {
         public float yPos;
