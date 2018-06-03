b6a7e127a376ec02d99a008296e6a40d1ec2b4e7
Joseph Lerner <josephalerner@gmail.com>
2018-06-03 16:17:28 -0700

    blocks fit nicely with terrain

--- Assets/BlockManager.cs
+++ Assets/BlockManager.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class BlockManager : MonoBehaviour {
 	public GameObject woodBlock;
 
-	int widthOfPendingBlock = 1;
-	int heightOfPendingBlock = 1;
+	int widthOfPendingBlock = 5;
+	int heightOfPendingBlock = 5;
 
 	// Use this for initialization
 	void Start () {
-		CreateBlock (4.3f);
+		CreateBlock (3.5f);
 	}
 
 	public void CreateBlock(float left) {
-		float yPosition = TerrainGenerator.instance.GetLowestHeight (left, widthOfPendingBlock);
+		float yPosition = TerrainGenerator.instance.GetLowestHeight (left, widthOfPendingBlock +1);
 		float xPosition = Mathf.RoundToInt(left);
 
-		GameObject newBlock = Instantiate (woodBlock, new Vector3 (xPosition, yPosition, 0), Quaternion.identity, this.transform);
+		GameObject newBlock = Instantiate (woodBlock, new Vector3 (xPosition + (widthOfPendingBlock * .5f), yPosition + (heightOfPendingBlock * .5f), 0), Quaternion.identity, this.transform);
 		Block block = newBlock.GetComponent<Block> ();
 		block.InitializeBlock (widthOfPendingBlock, heightOfPendingBlock);
 	}
