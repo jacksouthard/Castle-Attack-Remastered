ca826cd744a86a3bb248d4afbed4f5ece459b9f8
Joseph Lerner <josephalerner@gmail.com>
2018-06-03 15:03:46 -0700

    Created Block.cs

---
+++ Assets/Block.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+	public int width;
+	public int height;
+	public int maxHealth;
+	public int currentHealth;
+	public string buildingMaterialName;
+
+	public void InitializeBlock(float left, int width, int height) {
+		float yPosition = TerrainGenerator.instance.GetLowestHeight (left, width);
+		float xPosition = Mathf.RoundToInt(left);
+		this.transform.position = new Vector3 (xPosition, yPosition, 0);
+		currentHealth = maxHealth;
+	}
+}
