f3550d4b8b088f9dead87a43c3a9a6215d8635b5
Joseph Lerner <josephalerner@gmail.com>
2018-06-03 16:58:10 -0700

    added the block lerp

--- Assets/BlockManager.cs
+++ Assets/BlockManager.cs
@@ -3,22 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BlockManager : MonoBehaviour {
-	public GameObject woodBlock;
+	public static BlockManager instance;
+	public float lerpSpeed = 1f;
 
-	int widthOfPendingBlock = 5;
-	int heightOfPendingBlock = 5;
-
-	// Use this for initialization
-	void Start () {
-		CreateBlock (3.5f);
+	private void Awake() {
+		instance = this;
 	}
 
-	public void CreateBlock(float left) {
-		float yPosition = TerrainGenerator.instance.GetLowestHeight (left, widthOfPendingBlock +1);
-		float xPosition = Mathf.RoundToInt(left);
+	public GameObject woodBlock;
 
-		GameObject newBlock = Instantiate (woodBlock, new Vector3 (xPosition + (widthOfPendingBlock * .5f), yPosition + (heightOfPendingBlock * .5f), 0), Quaternion.identity, this.transform);
+	int widthOfPendingBlock = 1;
+	int heightOfPendingBlock = 1;
+
+	public void CreateBlock(float mouseXPosition, float mouseYPosition) {
+		float yPosition = TerrainGenerator.instance.GetLowestHeight (mouseXPosition, widthOfPendingBlock +1);
+		float xPosition = Mathf.RoundToInt(mouseXPosition);
+		Vector3 startPosition = new Vector3 (xPosition + (widthOfPendingBlock * .5f), mouseYPosition, 0);
+		Vector3 endPosition = new Vector3 (xPosition + (widthOfPendingBlock * .5f), yPosition + (heightOfPendingBlock * .5f), 0);
+		GameObject newBlock = Instantiate (woodBlock, startPosition, Quaternion.identity, this.transform);
 		Block block = newBlock.GetComponent<Block> ();
 		block.InitializeBlock (widthOfPendingBlock, heightOfPendingBlock);
+		StartCoroutine(LerpIt(block.transform, startPosition, endPosition));
+	}
+
+	IEnumerator LerpIt(Transform t, Vector3 startPosition, Vector3 endPosition) {
+		float startTime = Time.time;
+		float journeyLength = Vector3.Distance(startPosition, endPosition);
+
+		while (true) {
+			yield return new WaitForEndOfFrame ();
+			float distCovered = (Time.time - startTime) * lerpSpeed;
+			float fracJourney = distCovered / journeyLength;
+			t.position = Vector3.Lerp (startPosition, endPosition, fracJourney);
+		}
 	}
 }
---
+++ Assets/ClickManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour {
+	private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            float xPos = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+
+			if (xPos > TerrainGenerator.instance.mapWidth) {
+				return;
+			}
+
+			if (xPos < 0) {
+				return;
+			}
+
+			float yPos = Camera.main.ScreenToWorldPoint (Input.mousePosition).y;
+
+			BlockManager.instance.CreateBlock (xPos, yPos);
+        }
+	}
+}
